018fea48731bdb98e18b5d4c874b1c64b12ae5a9
Cindy <cindy.leschaud@gmail.com>
2015-09-15 15:18:36 +0200

    styles refresh

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo asset('frontend/css/guides.css');?>">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="<?php echo asset('schemas/Treant.css');?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo asset('frontend/css/schemas.css');?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo asset('frontend/css/schemas.css');?>?<?php echo rand(1234,120000); ?>">
 
 </head>
 
