49cf4dd1893c2968dd3ee27e35bdbd7b5c58af61
Cindy <cindy.leschaud@gmail.com>
2015-09-16 09:37:23 +0200

    external links open new tab

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -73,5 +73,16 @@ $(function(){
         con.css('overflow-y','hidden');
     }
 
+    $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+            $(this).click(function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.open(this.href, '_blank');
+            });
+        }
+    });
+
 });
 
